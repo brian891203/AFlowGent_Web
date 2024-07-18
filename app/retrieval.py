f565775780e3c323fbbcb0e5dba2b4517f8ae016
Brian <brian891203@gmail.com>
2024-07-18 17:42:40 +0800

    Initial commit

--- app/retrieval.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def render_next_node_page():
-    st.title("Next Node")
-    st.write("This is the next node page.")
-    if st.button("Go Back"):
-        st.session_state.page = "question_classification"
-        st.rerun()
